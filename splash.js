0e469191bc6be8ba6e54df4c79d69b11c074a386
Nemu64 <35722540+Nemu64@users.noreply.github.com>
2020-06-26 16:00:54 -0400

    rc5

---
+++ splash.js
@@ -0,0 +1,7 @@
+var quotes = ['Dancing with bears!', 'Reloading AK-47!', 'Imagining your smile!', 'Exploring the Nether!', 'Contemplating life!', 'Loading A LOT of data!', 'Rewriting history!'
+, 'Squashing bugs!', 'Waiting on your slow internet connection!' ];
+var length = quotes.length;
+var rand = Math.round(Math.random()*(length - 1));
+var one = ["Loading, please wait..."];
+var two = ["Please be patient..."];
+var three = ["All done!"]
